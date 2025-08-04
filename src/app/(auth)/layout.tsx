0e96e70cc14a98c07c835168237d8f5aa74bc866
sachinsinghshah <sachinsinghshah@gmail.com>
2025-08-04 23:26:13 +0530

    minor fix

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 const layout = ({ children }: Props) => {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p6 md:p-10">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <div>{children}</div>
       </div>
